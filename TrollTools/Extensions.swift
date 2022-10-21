5e518d56382296770db6b0498927941f78890c6e
llsc12 <llsc12.dev@gmail.com>
2022-10-21 17:17:33 +0100

    Tidy extensions, add respring extension

--- TrollTools/Extensions.swift
+++ TrollTools/Extensions.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension StringProtocol {
     subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
@@ -20,4 +21,32 @@ extension StringProtocol {
     subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
     subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
     subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
+}
+
+extension View {
+    /// Sets the text color for a navigation bar title.
+    /// - Parameter color: Color the title should be
+    ///
+    /// Supports both regular and large titles.
+    @available(iOS 14, *)
+    func navigationBarTitleTextColor(_ color: Color) -> some View {
+        let uiColor = UIColor(color)
+    
+        // Set appearance for both normal and large sizes.
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: uiColor ]
+        
+        return self
+    }
+}
+
+extension UIApplication {
+    public func respring() {
+        let app = self
+        // Credit to Amy While for this respring bug
+        guard let window = app.windows.first else { return }
+        while true {
+            window.snapshotView(afterScreenUpdates: false)
+        }
+    }
 }
