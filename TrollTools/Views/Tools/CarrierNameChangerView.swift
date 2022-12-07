5486137909c04d3d50fc88a1358775bdb04f1d79
leminlimez <jirizarry317@gmail.com>
2022-12-06 21:40:43 -0500

    new carrier changer ui

--- TrollTools/Views/Tools/CarrierNameChangerView.swift
+++ TrollTools/Views/Tools/CarrierNameChangerView.swift
@@ -9,17 +9,46 @@ import SwiftUI
 
 struct CarrierNameChangerView: View {
     @State var str: String = ""
+    @State private var carrierOffsetX: CGFloat = .zero
+    @State private var carrierOffsetY: CGFloat = .zero
     
     var body: some View {
-        TextField("Custom carrier text", text: $str)
-            .padding()
-        Button("Apply") {
-            do {
-                try CarrierNameManager.change(to: str)
-            } catch {
-                UIApplication.shared.alert(body: error.localizedDescription)
+        GeometryReader { proxy in
+            VStack {
+                Group {
+                    ZStack(alignment: .center) {
+                        Image("13cc")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .background(GeometryReader { (geometry) -> Color in
+                                DispatchQueue.main.async {
+                                    carrierOffsetX = -geometry.size.width/15
+                                    carrierOffsetY = -geometry.size.height/2 + (168/1294) * geometry.size.height
+                                }
+                                return .clear
+                            })
+                        TextField("carrier", text: $str)
+                            .offset(x: carrierOffsetX, y: carrierOffsetY)
+                            .frame(width: 125, height: 40, alignment: .center)
+                            .multilineTextAlignment(.leading)
+                    }
+                }
+                .frame(maxWidth: proxy.size.width * 0.9)
+                .padding()
+                
+                Button("Apply") {
+                    do {
+                        try CarrierNameManager.change(to: str)
+                    } catch {
+                        UIApplication.shared.alert(body: error.localizedDescription)
+                    }
+                }
+                .padding(.bottom)
             }
+            .ignoresSafeArea(.keyboard)
+            .frame(maxWidth: .infinity)
         }
+        .navigationTitle("Carrier Changer")
     }
 }
 
